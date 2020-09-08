21dafa4f2114dc19d09b3f6383995e7d7e542861
a.molinam <a.molinam@uniandes.edu.co>
2020-09-07 19:11:31 -0500

    Menu Creation

--- App/view.py
+++ App/view.py
@@ -38,9 +38,11 @@ operación seleccionada.
 # ___________________________________________________
 #  Ruta a los archivos
 # ___________________________________________________
-
-
-# ___________________________________________________
+allmovies = 'Movies/AllMoviesCastingRaw.csv'
+detailmovies = 'Movies/AllMoviesDetailsCleaned.csv'
+castingmovies = 'Movies/MoviesCastingRaw-small.csv'
+smallmovies = 'Movies/SmallMoviesDetailsCleaned.csv'
+# ___________________________________________________ 
 #  Funciones para imprimir la inforamación de
 #  respuesta.  La vista solo interactua con
 #  el controlador.
@@ -50,3 +52,37 @@ operación seleccionada.
 # ___________________________________________________
 #  Menu principal
 # ___________________________________________________
+
+def printMenu():
+    print("Bienvenido")
+    print("1- Cargar informacion del catalogo")
+    print("2- Mombres y cantidad de peliculas por productora")
+    print("3- Trabajos de un director")
+    print("4- Trabajos de un actor")
+    print("5- Peliculas por genero cinematografico")
+    print("6- Peliculas por pais")
+    print("0- Salir")
+
+
+while True:
+    print_menu()
+    inputs = input('Seleccione una opción para continuar')
+    if int(inputs[0]) == 1:
+        print("Cargando información de los archivos...")
+        controller.loadData(cont, allmovies, detailmovies, castingmovies, smallmovies)
+        print('Peliculas cargadas: ' + str(controller.detailmovies(cont)))
+        print('Actores cargados: ' + str(controller.castingmovies(cont)))
+    elif int(inputs[0]) == 2:
+        None
+    elif int(inputs[0]) == 3:
+        None
+    elif int(inputs[0]) == 4:
+        None
+    elif int(inputs[0]) == 5:
+        None
+    elif int(inputs[0]) == 6:
+        None
+    elif int(inputs[0]) == 0:
+        sys.exit(0)
+sys.exit(0)
+ 
