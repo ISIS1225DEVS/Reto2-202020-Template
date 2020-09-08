2d6f8ffe73e4ff02928e3bad81eddb87a6d1faff
sGao21 <s.gaona@uniandes.edu.co>
2020-09-08 13:55:25 -0500

    final

--- App/model.py
+++ App/model.py
@@ -25,17 +25,45 @@ from DISClib.ADT import map as mp
 from DISClib.DataStructures import mapentry as me
 assert config
 
+
+import config as cf
+import sys
+import csv
+
+from time import process_time 
+
+
+
 """
 En este archivo definimos los TADs que vamos a usar,
 es decir contiene los modelos con los datos en memoria
 
 """
-
+def compareRecordIds (recordA, recordB):
+    if int(recordA['id']) == int(recordB['id']):
+        return 0
+    elif int(recordA['id']) > int(recordB['id']):
+        return 1
+    return -1
 # -----------------------------------------------------
 # API del TAD Catalogo de Libros
 # -----------------------------------------------------
 
 
+def loadCSVFile (file, sep=";"):
+    lst = lt.newList("ARRAY_LIST") #Usando implementacion arraylist
+    #lst = lt.newList() #Usando implementacion linkedlist
+    print("Cargando archivo ....")
+    dialect = csv.excel()
+    dialect.delimiter=sep
+    try:
+        with open(file, encoding="utf-8") as csvfile:
+            spamreader = csv.DictReader(csvfile, dialect=dialect)
+            for row in spamreader: 
+                lt.addLast(lst,row)
+    except:
+        print("Hubo un error con la carga del archivo")
+    return lst
 
 # Funciones para agregar informacion al catalogo
 
