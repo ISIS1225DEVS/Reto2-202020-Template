6d220bad1bc5917c18dc3ce129d69d6d8003ec39
j.arroyo <j.arroyo@uniandes.edu.co>
2020-09-09 18:53:22 -0500

    Finalizado todo ahora sí

--- App/view.py
+++ App/view.py
@@ -37,8 +37,10 @@ operación seleccionada.
 # ___________________________________________________
 #  Ruta a los archivos
 # ___________________________________________________
-file1 = "MoviesCastingRaw-small.csv"
-file2 = "SmallMoviesDetailsCleaned.csv"
+# file1 = "MoviesCastingRaw-small.csv"
+# file2 = "SmallMoviesDetailsCleaned.csv"
+file1 = "AllMoviesCastingRaw.csv"
+file2 = "AllMoviesDetailsCleaned.csv"
 
 
 
@@ -65,25 +67,27 @@ while True:
     imprimirMenu()
     listaMovies = controller.iniciarCatalogo('ARRAY_LIST')
     seleccion = input("Seleccione una opción\n")
-    if int(seleccion[0]) == 1:
-        print("Cargando archivos...")
-        listaMovies = controller.cargarArchivos(file1,file2)
-        print("Se cargaron",listaMovies["size"],"películas\n")
-        el_in = lt.getElement(listaMovies,1)
-        el_fin = lt.getElement(listaMovies,0)
-        print("{:<21}{:<21}{:<21}{:<21}{:<21}".format("Título","Fecha de estreno","Votación promedio","Cantidad de votos","Idioma"))
-        print("{:<21}{:<21}{:<21}{:<21}{:<21}".format(el_in["title"],el_in["release_date"],el_in["vote_average"],el_in["vote_count"],el_in["spoken_languages"]))
-        print("{:<21}{:<21}{:<21}{:<21}{:<21}".format(el_fin["title"],el_fin["release_date"],el_fin["vote_average"],el_fin["vote_count"],el_fin["spoken_languages"]))
-    elif int(seleccion[0]) == 2:
-        pass
-    elif int(seleccion[0]) == 3:
-        pass
-    elif int(seleccion[0]) == 4:
-        pass
-    elif int(seleccion[0]) == 5:
-        pass
-    elif int(seleccion[0]) == 6:
-        pass
-    else:
-        sys.exit()
-    
+    try:
+        if int(seleccion[0]) == 1:
+            print("Cargando archivos...")
+            listaMovies = controller.cargarArchivos(file1,file2)
+            print("Se cargaron",listaMovies["size"],"películas\n")
+            el_in = lt.getElement(listaMovies,1)
+            el_fin = lt.getElement(listaMovies,0)
+            print("{:<21}{:<21}{:<21}{:<21}{:<21}".format("Título","Fecha de estreno","Votación promedio","Cantidad de votos","Idioma"))
+            print("{:<21}{:<21}{:<21}{:<21}{:<21}".format(el_in["title"],el_in["release_date"],el_in["vote_average"],el_in["vote_count"],el_in["spoken_languages"]))
+            print("{:<21}{:<21}{:<21}{:<21}{:<21}".format(el_fin["title"],el_fin["release_date"],el_fin["vote_average"],el_fin["vote_count"],el_fin["spoken_languages"]))
+        elif int(seleccion[0]) == 2:
+            pass
+        elif int(seleccion[0]) == 3:
+            pass
+        elif int(seleccion[0]) == 4:
+            pass
+        elif int(seleccion[0]) == 5:
+            pass
+        elif int(seleccion[0]) == 6:
+            pass
+        else:
+            sys.exit()
+    except:
+        sys.exit()    
