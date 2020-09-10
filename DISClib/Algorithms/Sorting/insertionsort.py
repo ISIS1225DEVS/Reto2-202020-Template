c4cadf1c0b15283a74b302548b80f04778737009
Blanca Valencia Aguirre <b.valenciaa@uniandes.edu.co>
2020-09-09 20:25:20 -0500

    Update insertionsort.py

--- DISClib/Algorithms/Sorting/insertionsort.py
+++ DISClib/Algorithms/Sorting/insertionsort.py
@@ -30,7 +30,7 @@ def insertionSort(lst, lessfunction):
     size = lt.size(lst)
     pos1 = 1
     while pos1 <= size:
-        pos2 = pos1
+        pos2=pos1
         while (pos2 > 1) and (lessfunction(
               (lt.getElement(lst, pos2), lt.getElement(lst, pos2-1)))):
             lt.exchange(lst, pos2, pos2-1)
